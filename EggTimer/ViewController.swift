1439fe221a29a050022c68030fa0f39627fc5622
Tissiana Alves <altissiana@gmail.com>
2020-06-02 12:22:46 -0600

    changed the title label

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -4,23 +4,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLabel: UILabel!
-    
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    
     var timer = Timer()
     var totalTime = 0
     var secondsPassed = 0
     
-
     @IBAction func hardnessSelected(_ sender: UIButton) {
                 
         timer.invalidate()
-        
         let hardness = sender.currentTitle!
-        
         totalTime = eggTimes[hardness]!
-        
         progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:
             #selector(updateTimer), userInfo: nil, repeats: true)
@@ -29,11 +25,8 @@ class ViewController: UIViewController {
     
     @objc func updateTimer() {
         if secondsPassed < totalTime {
-            
-            let percentageProgress = Float(secondsPassed) / Float(totalTime)
             secondsPassed += 1
-            progressBar.progress = Float(percentageProgress)
-            
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
